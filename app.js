c9624fa4455e3f702845732da9123abe6f4ea425
mcube25 <mcube3567@gmail.com>
2021-02-17 22:52:38 -0800

    linking node files

---
+++ app.js
@@ -0,0 +1,4 @@
+var http = require('http');                         //connect withb http
+var sql = require('mssql');                         //connect with sql
+var express = require('express');                   //connect with express
+var path = require('path');
