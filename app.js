0013e740335cfc7559049aed57aad3768502911b
mcube25 <mcube3567@gmail.com>
2021-02-22 23:12:04 -0800

    api call

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ var http = require('http');                         //connect withb http
 var sql = require('mssql');                         //connect with sql
 var express = require('express');                   //connect with express
 var path = require('path');
+
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+const products = {};
